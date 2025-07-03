d2fdb81874ba211483f6d419a148cd7b049a1a60
Kali-Venum <subhamdas.abcd@gmail.com>
2025-07-03 21:00:09 +0530

    working on employee creation.

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -1,20 +1,47 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { MasterService } from '../../services/master.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-employee',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css',
 })
 export class EmployeeComponent implements OnInit {
   managerList: any = [];
 
+  http = inject(HttpClient);
   masterService = inject(MasterService);
 
   isAddEmployeeOpen = signal<boolean>(false);
 
-  ngOnInit(): void {}
+  employeeForm: any = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    jobTitle: new FormControl(''),
+    manager: new FormControl(''),
+    salary: new FormControl(0),
+  });
+
+  handleAddEmployee() {
+    console.log(this.employeeForm.value, 'values');
+  }
+
+  gettingAllManagers() {
+    this.http
+      .get('http://localhost:9000/emp/api/v1/managers')
+      .subscribe((response: any) => {
+        this.managerList = [...response];
+      });
+
+  console.log(this.managerList, "managerList")
+  }
+
+  ngOnInit(): void {
+    this.gettingAllManagers();
+  }
 
   openAddNewEmploySection() {
     this.isAddEmployeeOpen.set(true);
